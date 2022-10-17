af69ae86586e853fcf65385533701e869c470a47
Dimitri Prima Nugraha <dimitriprimanugraha@gmail.com>
2022-10-17 22:20:16 +0700

    baru sedikit, lanjut besok

--- sehat_terus/urls.py
+++ sehat_terus/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from sehat_terus.views import show_homepage
+from sehat_terus.views import show_homepage, show_lurah_page
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_homepage, name='show_homepage'),
+    path('lurah-page/', show_lurah_page, name='show_lurah_page'),
 ]
