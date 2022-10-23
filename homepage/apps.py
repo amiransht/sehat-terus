b4827aed5e9cc13ff3811e4c1929dce250822102
amiransht <amira19nisrina@gmail.com>
2022-10-23 21:29:36 +0700

    homepage

--- homepage/apps.py
+++ homepage/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SehatTerusConfig(AppConfig):
+class HomepageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sehat_terus'
+    name = 'homepage'
