b848fcd27db67c512166ff1051574b49f240b4ed
monica.shannon <monica.shannon.ms@gmail.com>
2022-10-23 20:51:50 +0700

    perbaikan

--- faq_page/urls.py
+++ faq_page/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from faq_page.views import show_faqpage
-from .views import create_disc, del_post, del_rep, edit_post, forum, add_reply
 
-app_name = 'wishlist'
+app_name = 'faq_page'
 
 urlpatterns = [
     path('', show_faqpage, name='show_faqpage'),
