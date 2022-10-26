c33c9723917b8dea9fbabadc73d9397a5ed5b394
dimitripn <dimitri.prima@ui.ac.id>
2022-10-26 23:35:17 +0700

    Waiting for the authentication to be done

--- lurah_page/models.py
+++ lurah_page/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+GENDER_CHOICES = (
+    ('laki', 'Laki-laki'),
+    ('perempuan', 'Perempuan'),
+)
+
+STATUS_CHOICES = (
+    ('nogejala', 'Tanpa Gejala'),
+    ('gejalasedang', 'Gejala Sedang'),
+    ('gejalaberat', 'Gejala Berat'),
+)
+
+class DataPasien(models.Model):
+    nama = models.CharField(max_length=100)
+    umur = models.IntegerField()
+    gender = models.CharField(max_length=10, choices= GENDER_CHOICES)
+    status = models.CharField(max_length=20, choices= STATUS_CHOICES)
+    alamat = models.CharField(max_length=100)
--- lurah_page/views.py
+++ lurah_page/views.py
@@ -1,5 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.urls import reverse
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+
+import datetime
+
+from lurah_page.models import DataPasien
+from lurah_page.forms import DataPasienForm
+
 
 # Create your views here.
+
+@login_required(login_url='authentication/login/')
 def show_lurah_page(request):
-    return render(request, "lurah.html")
+    data_pasien = DataPasien.objects.filter(user=request.user)
+    context = {'data_pasien': data_pasien,
+               'user': request.user
+               }
+    return render(request, 'lurah.html', context)
+
+
+@login_required(login_url='authentication/login/')
+def add_pasien(request):
+    form = DataPasienForm()
+    if request.method == 'POST':
+        form = DataPasienForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.save()
+            return redirect('lurah_page:show_lurah_page')
+
+    context = {'form': form}
+    return render(request, 'add_pasien.html', context)
+
+
+@csrf_exempt
+def add_pasien_ajax(request):
+    if request.method == 'POST':
+        nama = request.POST.get('nama')
+        umur = request.POST.get('umur')
+        gender = request.POST.get('gender')
+        status = request.POST.get('status')
+        alamat = request.POST.get('alamat')
+        todo = DataPasien.objects.create(
+            nama=nama, umur=umur, gender=gender, status=status, alamat=alamat, user=request.user)
+
+        result = {
+            'fields': {
+                'nama': todo.nama,
+                'umur': todo.umur,
+                'gender': todo.gender,
+                'status': todo.status,
+                'alamat': todo.alamat,
+            },
+            'pk': todo.pk
+        }
+        return JsonResponse(result)
+
+
+@csrf_exempt
+def delete_pasien(request, id):
+    if request.method == 'DELETE':
+        task = get_object_or_404(DataPasien, id=id)
+        task.delete()
+    return HttpResponse(status=204)
+
+
+@login_required(login_url='authentication/login/')
+def show_json(request):
+    data_pasien = DataPasien.objects.filter(user=request.user)
+    data = serializers.serialize('json', data_pasien)
+    return HttpResponse(data, content_type='application/json')
