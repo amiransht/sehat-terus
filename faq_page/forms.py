329dc0654ec2301fe343ec12c6cbccbc122f21a1
monica.shannon <monica.shannon.ms@gmail.com>
2022-10-27 14:41:19 +0700

    semoga bisa

--- faq_page/forms.py
+++ faq_page/forms.py
@@ -1,1 +1,20 @@
 from django import forms
+from .models import Blog
+
+class Blog_Form(forms.Form):
+    title = forms.CharField(
+        max_length = 100,
+        widget = forms.TextInput(
+            
+        )
+    )
+
+    content = forms.CharField(
+        max_length = 1000,
+        widget = forms.Textarea(
+            
+        )
+    )
+
+
+
--- faq_page/models.py
+++ faq_page/models.py
@@ -1,6 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
 
-# Create your models here.
-
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
