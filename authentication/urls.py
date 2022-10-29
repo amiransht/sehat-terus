9ee1778d6862ea2eff968d8a11db097df14c60c6
refianys1678 <refianys1678@gmail.com>
2022-10-29 09:22:43 +0700

    benerin alur dan rute user, BELOM MERGE NAVBARNYA DIMS SAMA MIR

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from . import views
+from homepage.views import show_homepage
 
 app_name = 'authentication'
 
 urlpatterns = [
-    path('', views.index, name= 'index'),
+    path('', show_homepage, name= 'index'),
     path('login/', views.login, name= 'login'),
     path('logout/', views.logout_user, name= 'logout'),
     path('register/', views.register, name= 'register'),
