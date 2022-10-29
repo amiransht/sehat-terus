3009b054aca5b9c5a7ac3fee3d563c01520c6ad6
refianys1678 <refianys1678@gmail.com>
2022-10-29 11:52:28 +0700

    rapihin lagih

--- authentication/urls.py
+++ authentication/urls.py
@@ -9,9 +9,6 @@ urlpatterns = [
     path('login/', views.login, name= 'login'),
     path('logout/', views.logout_user, name= 'logout'),
     path('register/', views.register, name= 'register'),
-    path('home/', views.home, name= 'home'),
-    path('lurah/', views.lurah, name= 'lurah'),
-    path('nakes/', views.nakes, name= 'nakes'),
     path('profil/', views.profile, name='profil'),
     path('setting/', views.setting, name='setting'),
 ]
