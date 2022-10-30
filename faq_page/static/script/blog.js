826bb3c8f917ed8ccf24bee583310c381cabb5a6
monica.shannon <monica.shannon.ms@gmail.com>
2022-10-30 22:21:33 +0700

    styling faq, form, blog

--- faq_page/static/script/blog.js
+++ faq_page/static/script/blog.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    $.get("/faq-page/getblog", function (data) {
-        for (i = 0; i < data.length; i++) {
-            $(".row").append(`<div class="card col-lg-1 col-md-1 col-sm-1" style="width: 50rem;">
+  $.get("/faq-page/getblog", function (data) {
+    for (i = 0; i < data.length; i++) {
+      $(".cardblog").append(`<div class="card col-lg-1 col-md-1 col-sm-1" style="width: 50rem;">
     <div class="card-body">
       <h5 class="card-title">${data[i].fields.title}</h5>
       <p class="card-text">${data[i].fields.content}</p>
     </div>
   </div>`)
-        }
-    })
+    }
+  })
 })
