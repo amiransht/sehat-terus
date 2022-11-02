93c5a3b891b4a406f3427fca4410433c9415d6b0
refianys1678 <refianys1678@gmail.com>
2022-11-02 08:11:55 +0700

    benerin image src

--- faq_page/static/script/forms.js
+++ faq_page/static/script/forms.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#add-blog").click(function(a){
-    const result = $.post("/faq-page/submit_blog", {
+    $.post("/faq-page/submit_blog", {
       title: $('#id_title').val(),
       content: $('#id_content').val()}, 
       )
